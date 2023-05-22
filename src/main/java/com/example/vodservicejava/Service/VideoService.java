17996cc37cfbcf974c336ed45c825449e835857b
kabalport <kabalport@gmail.com>
2023-05-22 20:37:18 +0900

    video crud추가

--- src/main/java/com/example/vodservicejava/Service/VideoService.java
+++ src/main/java/com/example/vodservicejava/Service/VideoService.java
@@ -2,6 +2,9 @@ package com.example.vodservicejava.Service;
 
 import com.example.vodservicejava.Domain.Video;
 import com.example.vodservicejava.Repository.VideoRepository;
+import com.example.vodservicejava.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
@@ -20,13 +23,16 @@ import java.net.MalformedURLException;
 public class VideoService {
 
     private final VideoRepository videoRepository;
-    private final String uploadDir = "C:/upload/";
+//    private final String uploadDir = "C:/upload/";
+//    private final String uploadDir = "/Users/choedaeyeol/upload/";
+    @Value("${upload.dir}")
+    private String uploadDir;
 
     public VideoService(VideoRepository videoRepository) {
         this.videoRepository = videoRepository;
     }
-
-    public Video uploadFile(MultipartFile file) throws IOException {
+    @Transactional
+    public Video uploadVideo(MultipartFile file) throws IOException {
         String fileName = file.getOriginalFilename();
         File dest = new File(uploadDir + fileName);
         file.transferTo(dest);
@@ -36,10 +42,50 @@ public class VideoService {
         video.setFilePath(dest.getAbsolutePath());
         return videoRepository.save(video);
     }
-
+    @Transactional(readOnly = true)
     public Resource getVideo(Long id) throws MalformedURLException {
         Video video = videoRepository.findById(id).orElseThrow();
         return new UrlResource(Paths.get(video.getFilePath()).toUri());
     }
+
+    @Transactional
+    public Video updateVideo(Long id, MultipartFile file) throws IOException {
+        Video video = videoRepository.findById(id)
+                .orElseThrow();
+
+        // Delete the old file
+        File oldFile = new File(video.getFilePath());
+        if(oldFile.exists()){
+            oldFile.delete();
+        }
+
+        String fileName = file.getOriginalFilename();
+        File dest = new File(uploadDir + fileName);
+        file.transferTo(dest);
+
+        video.setFileName(fileName);
+        video.setFilePath(dest.getAbsolutePath());
+        return videoRepository.save(video);
+    }
+
+    @Transactional
+    public void deleteVideo(Long id) {
+        Video video = videoRepository.findById(id)
+                .orElseThrow();
+        // 실제 파일도 삭제해야 합니다.
+        File file = new File(video.getFilePath());
+        if(file.exists()){
+            if(!file.delete()){
+                throw new RuntimeException("Failed to delete the file");
+            }
+        }
+        videoRepository.delete(video);
+    }
+    @Transactional(readOnly = true)
+    public List<Video> getAllVideos() {
+        return videoRepository.findAll();
+    }
+
+
 }
 
